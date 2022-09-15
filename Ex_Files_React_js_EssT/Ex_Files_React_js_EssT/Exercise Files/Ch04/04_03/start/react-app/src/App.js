146045e0b6fa0db789089c5735358790239782d7
David Whipple <dwhipp88@gmail.com>
2022-09-14 20:15:13 -0600

    destructed objects and destructured arrays using array destructuring

--- Ex_Files_React_js_EssT/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
+import './App.css';
 
-function App() {
+const [firstCity, secondCity, thirdCity] = ['Tokyo', 'Rome', 'Venice'];
+
+console.log(firstCity, secondCity, thirdCity);
+
+function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
