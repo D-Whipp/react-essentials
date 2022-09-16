9dfbead4d8ff65bb52e18ba308069f6945764f74
David Whipple <dwhipp88@gmail.com>
2022-09-15 21:39:22 -0600

    setup and passed arguments into a dependency array

--- Ex_Files_React_js_EssT/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/start/react-app/src/App.js
@@ -3,18 +3,25 @@ import { useState, useEffect } from 'react';
 
 function App({ library }) {
   const [emotion, setEmotion] = useState('happy');
+  const [secondary, setSecondary] = useState('tired');
 
   useEffect(() => {
     console.log(`It's ${emotion} right now.`);
   }, [emotion]);
   console.log(emotion);
 
+  useEffect(() => {
+    console.log(`It's ${secondary} around here.`);
+  }, [secondary]);
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion('sad')}>sad</button>
       <button onClick={() => setEmotion('excited')}>excited</button>
       <button onClick={() => setEmotion('joyous')}>joyous</button>
+      <h2>Current energy level is {secondary}</h2>
+      <button onClick={() => setSecondary('rested')}>rested</button>
+      <button onClick={() => setSecondary('tired')}>tired</button>
     </div>
   );
 }
